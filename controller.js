ec8115d3b6d717c0db4251335c12fd0839088686
akroll1 <andrew.kroll@yahoo.com>
2015-11-19 15:30:36 -0500

    got object back
    linked controller, got object back.

--- controller.js
+++ controller.js
@@ -1,9 +1,17 @@
-var twitterApp = angular.module('twitterApp', ['ngRoute']);
+var twitterApp = angular.module('twitterApp', []);
 	
-	module.controller('twitterController', function($scope, $http){
+	twitterApp.controller('twitterController', function($scope, $http){
 		var url = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=women';
 		$http.get(url).success(function(trumpData){
 			$scope.trumpQuotes = trumpData;
+			console.log(trumpData);
+			console.log()
 		});
+
+	
+
+
+
+
 
 	});
