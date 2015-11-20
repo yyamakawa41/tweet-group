2d489fed01b68d8c0b24d62818cfe7a82c560f3d
akroll1 <andrew.kroll@yahoo.com>
2015-11-20 14:48:26 -0500

    trying to push to fixed master.
    trying

--- controller.js
+++ controller.js
@@ -1,26 +1,18 @@
 var twitterApp = angular.module('twitterApp', []);
 	
 	twitterApp.controller('twitterController', function($scope, $http){
-		var url = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=women';
+		
+		$scope.secondHashTrumpQuotes = function(tag){
+			var url = 'http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash='+tag;
+
+			
 		$http.get(url).success(function(trumpData){
 			$scope.trumpQuotes = trumpData.statuses;
-			// console.log(trumpQuotes);
-			// console.log(trumpQuotes.statuses);
-			// console.log(trumpQuotes.statuses[0].user.screen_name);
-			// console.log(trumpQuotes.statuses[0].text);
-			// console.log(trumpQuotes.statuses[0].user.profile_banner_url);
-
-
-			// $scope.trumpTweets = trumpQuotes;
-
-			// for(i=0;i<$scope.trumpTweets.length;i++){
-			// 	$scope.names = (trumpTweets[i].user.screen_name);
-			// 	console.log(trumpTweets[i].user.screen_name);
-			// 	$scope.texts = (trumpTweets[i].text);
-			// 	$scope.image = (trumpTweets[i].user.profile_banner_url);
+			
 
 			});
 
+		}		
 		});
 
 	
@@ -29,4 +21,3 @@ var twitterApp = angular.module('twitterApp', []);
 
 
 
-	// });
